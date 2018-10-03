bc6eceee873444bdab56f694d5d725eb2ac5ee1c
Stefan Widgren <stefan.widgren@gmail.com>
2018-10-03 18:57:56 +0200

    Add tests for html headings

--- tests/test.R
+++ tests/test.R
@@ -33,10 +33,53 @@ stopifnot(identical(h, cars))
 
 h <- html_table(cars[1:2, ], tfoot = TRUE)
 h_expected <- "<table><thead><tr><th>speed</th><th>dist</th></tr></thead><tfoot><tr><td>4</td><td>10</td></tr></tfoot><tbody><tr><td>4</td><td>2</td></tr></tbody></table>"
-h_observed <- html(h)
+h_observed <- html(h, pretty = FALSE)
 stopifnot(identical(h_observed, h_expected))
 
 h <- html_table(cars[1:2, ])
 h_expected <- "<table><thead><tr><th>speed</th><th>dist</th></tr></thead><tbody><tr><td>4</td><td>2</td></tr><tr><td>4</td><td>10</td></tr></tbody></table>"
-h_observed <- html(h)
+h_observed <- html(h, pretty = FALSE)
 stopifnot(identical(h_observed, h_expected))
+
+## HTML headings
+h1_expected <- structure(list(tag = "H1",
+                              content = "Hello world!",
+                              attributes = list()),
+                         class = c("html_H1", "html_object"))
+h1_observed <- html_H1("Hello world!")
+stopifnot(identical(h1_observed, h1_expected))
+
+h2_expected <- structure(list(tag = "H2",
+                              content = "Hello world!",
+                              attributes = list()),
+                         class = c("html_H2", "html_object"))
+h2_observed <- html_H2("Hello world!")
+stopifnot(identical(h2_observed, h2_expected))
+
+h3_expected <- structure(list(tag = "H3",
+                              content = "Hello world!",
+                              attributes = list()),
+                         class = c("html_H3", "html_object"))
+h3_observed <- html_H3("Hello world!")
+stopifnot(identical(h3_observed, h3_expected))
+
+h4_expected <- structure(list(tag = "H4",
+                              content = "Hello world!",
+                              attributes = list()),
+                         class = c("html_H4", "html_object"))
+h4_observed <- html_H4("Hello world!")
+stopifnot(identical(h4_observed, h4_expected))
+
+h5_expected <- structure(list(tag = "H5",
+                              content = "Hello world!",
+                              attributes = list()),
+                         class = c("html_H5", "html_object"))
+h5_observed <- html_H5("Hello world!")
+stopifnot(identical(h5_observed, h5_expected))
+
+h6_expected <- structure(list(tag = "H6",
+                              content = "Hello world!",
+                              attributes = list()),
+                         class = c("html_H6", "html_object"))
+h6_observed <- html_H6("Hello world!")
+stopifnot(identical(h6_observed, h6_expected))
